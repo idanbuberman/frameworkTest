9e20f533fdf09d15ce7ae639ba35572be1156853
Idan Boberman <idanbo@perfectomobile.com>
2019-01-03 18:06:38 +0200

    Initial Commit

---
+++ PMPrometheusClient/PMPrometheusClient.h
@@ -0,0 +1,19 @@
+//
+//  PMPrometheusClient.h
+//  PMPrometheusClient
+//
+//  Created by Idan Boberman on 03/01/2019.
+//  Copyright © 2019 Idan Boberman. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PMPrometheusClient.
+FOUNDATION_EXPORT double PMPrometheusClientVersionNumber;
+
+//! Project version string for PMPrometheusClient.
+FOUNDATION_EXPORT const unsigned char PMPrometheusClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PMPrometheusClient/PublicHeader.h>
+
+
